f676d0cca3e50e4878bc9e20fcb2372c35a4149b
Fabri <fabrizzioper@gmail.com>
2025-01-07 09:53:00 -0500

    MEJORAS VIUSALES

--- mi-portfolio/src/App.js
+++ mi-portfolio/src/App.js
@@ -1,28 +1,59 @@
-// src/App.js
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+// // src/App.js
+// import React, { useState, useEffect } from 'react';
+// import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+
+// const App = () => {
+//   const [isDark, setIsDark] = useState(true);
+
+//   const toggleDarkMode = () => {
+//     setIsDark(prev => !prev);
+//   };
+
+//   // Aplicar o eliminar la clase 'dark' al <html> para habilitar el modo oscuro de Tailwind
+//   useEffect(() => {
+//     if (isDark) {
+//       document.documentElement.classList.add('dark');
+//     } else {
+//       document.documentElement.classList.remove('dark');
+//     }
+//   }, [isDark]);
+
+//   return (
+//     <div className={`transition-colors duration-300 ${isDark ? 'dark' : ''}`}>
+//       <Navbar isDark={isDark} toggleDarkMode={toggleDarkMode} />
+//       <Hero isDark={isDark} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import Portfolio from './components/Portfolio';
+import './index.css';
 
 const App = () => {
-  const [isDark, setIsDark] = useState(true);
-
-  const toggleDarkMode = () => {
-    setIsDark(prev => !prev);
-  };
-
-  // Aplicar o eliminar la clase 'dark' al <html> para habilitar el modo oscuro de Tailwind
-  useEffect(() => {
-    if (isDark) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [isDark]);
 
   return (
-    <div className={`transition-colors duration-300 ${isDark ? 'dark' : ''}`}>
-      <Navbar isDark={isDark} toggleDarkMode={toggleDarkMode} />
-      <Hero isDark={isDark} />
+    <div className="relative w-screen h-screen bg-gray-900 overflow-hidden">
+      <Portfolio />
     </div>
   );
 };
